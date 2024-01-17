bfb5b88276bebf2ca6e1a3a00a70ce8802e19279
namng <nam.nn@bidgear.com>
2024-01-17 22:30:32 +0700

    edit

--- lode_api/urls.py
+++ lode_api/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('authentication/', include('authentication.urls')),
+    path('auth/', include('authentication.urls')),
     path('', include('server.urls')),
     path('api/order/', include('gameplay.urls'))
 ]
