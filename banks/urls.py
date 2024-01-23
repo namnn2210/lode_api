11e4eead6ca001a4e33b5d16372f4d12c744ad61
namng <nam.nn@bidgear.com>
2024-01-24 00:46:21 +0700

    edit

--- banks/urls.py
+++ banks/urls.py
@@ -3,5 +3,6 @@ from .views import fetch_banks, BankView
 
 urlpatterns = [
     path('fetch', fetch_banks, name='fetch_banks'),
-    path('banks', BankView.as_view(), name='banks'),
+    path('banks', BankView.as_view(), name='banks-list'),
+    path('banks/<int:bank_id>', BankView.as_view(), name='banks-view'),
 ]
--- banks/views.py
+++ banks/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from server.models import APIResponse
-
+from rest_framework import status
 from .serializer import BankSerializer
 import requests
 
@@ -27,3 +27,37 @@ class BankView(APIView):
         all_banks = Bank.objects.all()
         bank_serializers = BankSerializer(all_banks, many=True).data
         return Response(APIResponse(success=True, data=bank_serializers, message="").__dict__())
+
+    def post(self, request):
+        serializer = BankSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(APIResponse(success=True, data=serializer.data, message="").__dict__(),
+                            status=status.HTTP_201_CREATED)
+        return Response(APIResponse(success=False, data={}, message="Lưu thông tin thất bại").__dict__(),
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, bank_id):
+        try:
+            bank = Bank.objects.get(pk=bank_id, status=True)
+        except Bank.DoesNotExist:
+            return Response(APIResponse(success=False, data={}, message="Dữ liệu không tồn tại").__dict__(),
+                            status=status.HTTP_404_NOT_FOUND)
+
+        serializer = BankSerializer(bank, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(APIResponse(success=True, data=serializer.data, message="").__dict__())
+        return Response(APIResponse(success=False, data={}, message="Lưu thông tin thất bại").__dict__(),
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, bank_id):
+        try:
+            bank = Bank.objects.get(pk=bank_id, status=True)
+        except Bank.DoesNotExist:
+            return Response(APIResponse(success=False, data={}, message="Dữ liệu không tồn tại").__dict__(),
+                            status=status.HTTP_404_NOT_FOUND)
+
+        bank.status = False
+        bank.save()
+        return Response(APIResponse(success=True, data={}, message="Xóa thành công").__dict__())
